55f8d43f7b8e919f5200f29445ea73fd95d00af9
jahnavigajjala3-commits <jahnavigajjala3@gmail.com>
2026-08-07 18:49:14 +0530

    feat(api): define initial HealthReport CRD

--- api/v1alpha1/healthreport_types.go
+++ api/v1alpha1/healthreport_types.go
@@ -26,37 +26,24 @@ import (
 
 // HealthReportSpec defines the desired state of HealthReport
 type HealthReportSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-	// The following markers will use OpenAPI v3 schema to validate the value
-	// More info: https://book.kubebuilder.io/reference/markers/crd-validation.html
 
-	// foo is an example field of HealthReport. Edit healthreport_types.go to remove/update
-	// +optional
-	Foo *string `json:"foo,omitempty"`
+	
 }
 
 // HealthReportStatus defines the observed state of HealthReport.
 type HealthReportStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	
+    Namespace string `json:"namespace,omitempty"`
+    PodName string `json:"podName,omitempty"`
+    Phase string `json:"phase,omitempty"`
+    Diagnosis string `json:"diagnosis,omitempty"`
+    Message string `json:"message,omitempty"`
+    RestartCount int32 `json:"restartCount,omitempty"`
+	LastUpdated metav1.Time `json:"lastUpdated,omitempty"`
+    Recommendation string `json:"recommendation,omitempty"`
+    Severity string `json:"severity,omitempty"`
+    Conditions []metav1.Condition `json:"conditions,omitempty"`
 
-	// For Kubernetes API conventions, see:
-	// https://github.com/kubernetes/community/blob/master/contributors/devel/sig-architecture/api-conventions.md#typical-status-properties
-
-	// conditions represent the current state of the HealthReport resource.
-	// Each condition has a unique type and reflects the status of a specific aspect of the resource.
-	//
-	// Standard condition types include:
-	// - "Available": the resource is fully functional
-	// - "Progressing": the resource is being created or updated
-	// - "Degraded": the resource failed to reach or maintain its desired state
-	//
-	// The status of each condition is one of True, False, or Unknown.
-	// +listType=map
-	// +listMapKey=type
-	// +optional
-	Conditions []metav1.Condition `json:"conditions,omitempty"`
 }
 
 // +kubebuilder:object:root=true
